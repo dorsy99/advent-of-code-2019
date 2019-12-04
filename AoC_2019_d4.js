c9bbbe76025e2b87c3e57a631f06bf698485868e
Andrew Albury-Dor <dorsy99@gmail.com>
2019-12-04 15:07:23 +1000

    day4 init

---
+++ AoC_2019_d4.js
@@ -0,0 +1,32 @@
+/////////////
+// Day 4!
+/////////////
+
+var input = "128392 - 643281";
+var range = [];
+
+for (var i = 128392; i < 643282; i++) {
+    range.push(i);
+}
+
+function hasConsecutiveNums(input) {
+
+}
+
+
+console.log(range);
+
+
+
+
+/* 
+var arr = ['a', 'b', 'a', 'c', 'a', 'd'];
+arr = arr.filter(
+    function (value, index, self) {
+        return self.indexOf(value) === index;
+    }
+)
+console.log(arr);
+outputs:
+[ 'a', 'b', 'c', 'd' ]
+*/
