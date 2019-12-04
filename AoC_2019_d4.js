345c35c6b0d8f2dac0163eab1ab6ed8293dfadac
Andrew Albury-Dor <dorsy99@gmail.com>
2019-12-04 15:52:57 +1000

    part 1 done, starting part 2

--- AoC_2019_d4.js
+++ AoC_2019_d4.js
@@ -10,14 +10,46 @@ for (var i = 128392; i < 643282; i++) {
 }
 
 function hasConsecutiveNums(input) {
+    //var inputSplit = input.split("");
 
+    //'/\d{4}/'
+
+    /*inputSplit.filter(function(value, index){
+        if (inputSplit[index+1] == value) {
+            return true;
+        } else {
+            return false;
+        }
+    })*/
+
+    return (!!(input.toString().match(/.*(?:(\d)\1{1})/)));
 }
 
+function onlyIncreases(input) {
+    var inputSplit = input.toString();
+    var output = true;
+    for (i = 0; i < 6; i++) {
+        if (inputSplit[i] > inputSplit[i + 1]) {
+            output = false;
+        }
+    }
+    return output;
+}
 
+/*
+console.log(onlyIncreases("12344"));
+
+console.log();
+*/
+var answer = 0;
+range.forEach(function(item) {
+    if (onlyIncreases(item) && hasConsecutiveNums(item)) {
+        answer++;
+    }
+})
+
+console.log("ANSWER IS: " + answer);
 console.log(range);
-
-
-
 
 /* 
 var arr = ['a', 'b', 'a', 'c', 'a', 'd'];
